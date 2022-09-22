f081a800a1f2db04942a86d207c5d2f285fbcb81
Halász Miklós <miklos2.halasz@kh.hu>
2022-09-22 20:58:16 +0200

    implements about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,33 @@
 <template>
-  <h1>Hi there! :-)</h1>
+  <h1>{{ user.username }}</h1>
+  <p>{{ user.description }}</p>
+  <post-list
+    v-bind:posts="posts"
+    title="Last Posts"></post-list>
 </template>
 
 <script>
+import PostList from '@/components/posts/PostList'
+
 export default {
-  name: 'AboutView'
+  components: { PostList },
+  name: 'AboutView',
+  data: function () {
+    return {
+      user: {
+        username: 'teszt.elek',
+        description: 'Hi! I\'m the test user here!'
+      },
+      posts: [
+        {
+          id: 1,
+          user: 'teszt.elek',
+          date: '2022-09-21T20:00:00',
+          post: 'Hello, World!'
+        }
+      ]
+    }
+  }
 }
 </script>
 
