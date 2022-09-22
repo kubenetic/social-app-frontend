a0d27e92fc2f3477c140a830026b543320b38147
Halász Miklós <miklos2.halasz@kh.hu>
2022-09-22 20:57:59 +0200

    use conditional title

--- src/components/posts/PostList.vue
+++ src/components/posts/PostList.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="post-list">
-    <single-post
-      v-for="post in posts"
-      v-bind:key="post.id"
-      v-bind:post="post"/>
-  </div>
+  <h2 v-if="title">{{ title }}</h2>
+  <single-post
+    v-for="post in posts"
+    v-bind:key="post.id"
+    v-bind:post="post"/>
 </template>
 
 <script>
 import SinglePost from '@/components/posts/SinglePost'
+
 export default {
   name: 'PostList',
   components: { SinglePost },
   props: {
+    title: String,
     posts: {
       type: Object,
       required: true
