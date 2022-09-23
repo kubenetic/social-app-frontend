3887e622e55ad563270592bae8a981748816222a
Halász Miklós <miklos2.halasz@kh.hu>
2022-09-23 18:10:35 +0200

    scaffold LoginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <form>
+    <label for="username">Username</label>
+    <input type="text" id="username">
+
+    <label for="password">Password</label>
+    <input type="password" id="password">
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView'
+}
+</script>
+
+<style scoped>
+
+</style>
